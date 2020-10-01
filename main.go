f6fa3e05277b9d9a9ce571edeab0650f8b871c70
Vitaly Kashtalyan <vitaly.kashtalyan@gmail.com>
2020-10-01 21:41:15 +0300

    Added devices

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type devices []struct {
+	Id     uint   `json:"id"`
+	Name   string `json:"name"`
+	Host   string `json:"host"`
+	Enable bool   `json:"enable"`
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": http.StatusText(http.StatusOK),
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
+	r.GET("/devices", func(c *gin.Context) {
+		c.JSON(http.StatusOK, getDevices())
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func getDevices() devices {
+	jsonFile, err := os.Open("devices.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var d devices
+	_ = json.Unmarshal(byteValue, &d)
+	return d
 }
