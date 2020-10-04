c34fc7aafb2689429ea0780fe19b8f2bbe704d4e
Vitaly Kashtalyan <vitaly.kashtalyan@gmail.com>
2020-10-04 09:46:24 +0300

    Added docker maintain

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # go-manage-sonoff
+
+
+### SONOFF: BASICR3/RFR3/MINI HTTP API
+
+http://developers.sonoff.tech/basicr3-rfr3-mini-http-api.html
--- main.go
+++ main.go
@@ -9,14 +9,25 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"strconv"
 )
 
 type devices []struct {
-	Id     int    `json:"id"`
-	Name   string `json:"name"`
-	Host   string `json:"host"`
-	Enable bool   `json:"enable"`
+	DeviceId string `json:"id" binding:"required"`
+	Name     string `json:"name"`
+	Host     string `json:"host" binding:"required"`
+}
+
+type errorMsg struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func init() {
+	_, isPresent := os.LookupEnv("DEVICES_FILE")
+	if !isPresent {
+		_ = os.Setenv("DEVICES_FILE", "devices.json")
+	}
+	fmt.Println("DEVICES_FILE:", os.Getenv("DEVICES_FILE"))
 }
 
 func main() {
@@ -28,7 +39,15 @@ func main() {
 	})
 
 	r.GET("/devices", func(c *gin.Context) {
-		c.JSON(http.StatusOK, getDevices())
+		devices, err := getDevices()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, errorMsg{
+				Status:  http.StatusText(http.StatusInternalServerError),
+				Message: err.Error(),
+			})
+		} else {
+			c.JSON(http.StatusOK, devices)
+		}
 	})
 
 	r.POST("/device/:id/*proxyPath", proxy)
@@ -36,24 +55,54 @@ func main() {
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
-func getDevices() devices {
-	jsonFile, err := os.Open("devices.json")
+func getDevices() (d devices, err error) {
+	jsonFile, err := os.Open(os.Getenv("DEVICES_FILE"))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("error opening file ["+os.Getenv("DEVICES_FILE")+"]:", err)
+		return
 	}
 	defer jsonFile.Close()
-	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-	var d devices
-	_ = json.Unmarshal(byteValue, &d)
-	return d
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("error reading JSON:", err)
+		return
+	}
+
+	err = json.Unmarshal(byteValue, &d)
+	if err != nil {
+		fmt.Println("error mapping from JSON to object:", err)
+		return
+	}
+	return
 }
 
 func proxy(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-	remote, err := url.Parse("http://" + getHostById(id))
+	host, err := getHostById(c.Param("id"))
 	if err != nil {
-		panic(err)
+		c.JSON(http.StatusInternalServerError, errorMsg{
+			Status:  http.StatusText(http.StatusInternalServerError),
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if host == "" {
+		c.JSON(http.StatusBadRequest, errorMsg{
+			Status:  http.StatusText(http.StatusBadRequest),
+			Message: "invalid device id: " + c.Param("id"),
+		})
+		return
+	}
+
+	remote, err := url.Parse("http://" + host)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, errorMsg{
+			Status:  http.StatusText(http.StatusBadRequest),
+			Message: err.Error(),
+		})
+		return
 	}
 
 	proxy := httputil.NewSingleHostReverseProxy(remote)
@@ -64,15 +113,17 @@ func proxy(c *gin.Context) {
 		req.URL.Host = remote.Host
 		req.URL.Path = c.Param("proxyPath")
 	}
-
 	proxy.ServeHTTP(c.Writer, c.Request)
 }
 
-func getHostById(id int) string {
-	for _, device := range getDevices() {
-		if device.Id == id {
-			return device.Host
+func getHostById(id string) (host string, err error) {
+	devices, err := getDevices()
+	if err == nil {
+		for _, device := range devices {
+			if device.DeviceId == id {
+				host = device.Host
+			}
 		}
 	}
-	return "127.0.0.1"
+	return
 }
